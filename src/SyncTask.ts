f56b0228317dd10edfecc011071f1f05e730c739
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-05-19 21:23:06 +0900

    Report Worker to Task, and report sheet

--- src/SyncTask.ts
+++ src/SyncTask.ts
@@ -13,6 +13,11 @@ function SyncWorkerToPart() : void {
   const activeSheet = ss.getActiveSheet()
   const startRange = ss.getRangeByName(activeSheet.getName()+'!작업자데이터시작');
   const syncData = getSyncData(startRange)
+
+  syncData.forEach(data=>{
+    syncAndReportWorkerTask(data)
+  })
+
 }
 
 function getSyncData(startRange : Range) : any[]{
@@ -20,4 +25,35 @@ function getSyncData(startRange : Range) : any[]{
   const dataStartColumn = startRange.getColumn()
   const syncData = startRange.getSheet().getRange(dataStartRow, dataStartColumn, getCutCount(), startRange.getLastColumn()-dataStartColumn+1)
   return syncData.getValues()
+}
+
+function syncAndReportWorkerTask(record : any[]){
+
+  const part = record[FieldOffset.PART]
+  if(!part){
+    console.log("파트가 없습니다.")
+    return;
+  }
+
+  const manageSpreadsheet = getSpreadsheet()
+  const partSheet = getSheetByName(part + " 파트")
+  if(!partSheet){
+    throw new Error("파트 시트가 없습니다.")
+  }
+
+  const numFieldName = FieldName.NUMBER+'필드'
+  const sameRecordRow = findSameRecordRow(record, manageSpreadsheet, partSheet, numFieldName)
+  if(sameRecordRow!=-1){
+    overwriteRecord(record, manageSpreadsheet, partSheet, numFieldName, sameRecordRow)
+    reportWorkerTask(record)
+    return;
+  }
+}
+
+function reportWorkerTask(record : any[]){
+  const manageSpreadsheet = getSpreadsheet()
+  const reportSheet = getSheetByName("로그")
+  const numFieldName = '로그'+FieldName.NUMBER+'필드'
+  const insertRow = reportSheet.getLastRow() + 1
+  insertRecord(record, manageSpreadsheet, reportSheet, numFieldName, insertRow)
 }
